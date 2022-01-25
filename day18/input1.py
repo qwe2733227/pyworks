1d993f7746282e1b557c0a1ccfc24d298eb3a83b
qwe2733227 <qwe2733227@naver.com>
2022-01-25 11:05:58 +0900

    day18

---
+++ day18/input1.py
@@ -0,0 +1,24 @@
+#입력 함수 - input()
+'''
+print("문자 입력:")
+ch = input()
+
+
+ch = input("문자입력:")
+
+print(ch)
+'''
+#숫자 입력
+'''
+num = input("숫자 입력 : ") #현재 문자가 입력
+num = int(num)              #문자를 숫자로 변환
+print(num + 1)
+'''
+
+num = int(input("숫자 입력 : "))
+print(num + 1)
+print(num > 10)
+
+num2 = float(input("숫자 입력 : "))
+print(num2 + 1)
+print(num2 > 10.1)
---
+++ day18/op_logic.py
@@ -0,0 +1,15 @@
+#논리 연산자
+
+x = 10
+y = -10
+
+print(x > 0) #True
+print(y > 0) #False
+print(x != y)
+print()
+
+
+print( x > 0 and y > 0) #False
+# print( x > 0 && y > 0)  '&&' '||' 사용불가
+print( x > 0 or y > 0)  #True
+print(not y > 0)        #True
