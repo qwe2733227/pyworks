e7c4f55462fcc0be7852d0ea30466f80e00ce1e0
qwe2733227 <qwe2733227@naver.com>
2022-01-25 12:48:58 +0900

    day18

---
+++ day18/if_example/limit_speed.py
@@ -0,0 +1,10 @@
+#if문
+#변수 생성 및 저장
+limit_speed = 40
+
+#if문
+if limit_speed >= 50:
+    print("안전속도 위반! 과태료 부과 대상")
+
+#출력
+print("시속 : " + str(limit_speed) + "km입니다.")
---
+++ day18/while_ex/while_3times.py
@@ -0,0 +1,8 @@
+result = 0
+i = 1
+while i <= 1000:
+    if i % 3 == 0: #3의 배수이면
+        result += i  # 누적 합계
+    i += 1
+
+print(result)
