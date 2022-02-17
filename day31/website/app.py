720d6dfcbb6108a6889ab768a4b0a706aec367e9
qwe2733227 <qwe2733227@naver.com>
2022-02-17 12:48:10 +0900

    flask web

--- day31/website/app.py
+++ day31/website/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template #템블릿 = html이라는 뜻
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -20,5 +20,45 @@ def season():
 def loop_index():
     items = ['a', 'b', 'c', 'd']
     return render_template('loop_index.html', items=items)
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # 데이터 전달 받음
+        mid = request.form['mid']
+        pwd = request.form['passwd']
+        name = request.form['name']
+        gender = request.form['gender']
+
+        # 데이터를 memberlist에 보냄
+        return render_template(
+            'memberlist.html',
+            mid = mid,
+            pwd = pwd,
+            name = name,
+            gender = gender
+        )
+    else:  #request.method == 'GET'
+        return render_template('register.html')
+
+@app.route('/even_odd', methods=['GET', 'POST'])
+def even_odd():
+    if request.method == 'POST':
+        try:
+        #데이터 수집
+            num = int(request.form['num']) #문자를 숫자로 형변환
+        except ValueError:
+            error = "숫자를 입력하세요"
+            return render_template('even_odd.html', error_msg = error)
+        else:
+        # 데이터 연산
+            if num % 2 == 0:
+                result = "짝수입니다."
+            else:
+                result = "홀수입니다."
+            #데이터 보내기
+            return render_template('calc_result.html',num=num,result = result)
+    else:
+        return render_template('even_odd.html')
 
 app.run()
